20fe2a9d54b0eaea4c3c2386dac7e4b26e7b26c0
Abir Haque <abir.haque.usa@gmail.com>
2022-04-09 12:29:46 -0500

    Pretty

--- public/js/studentportal.js
+++ public/js/studentportal.js
@@ -6,7 +6,19 @@ fetch("http://localhost:3000/getproblems")
            for (let i = 0; i< data.length;i++)
            {
               var id = data[i]._id
+              var date = data[i].date
               console.log(id)
-              document.getElementById("ayylmao").innerHTML+="<a href='/problem?id=" +id+"'>"+id+"</a><br>";
+
+
+              /*var dd = String(date.getDate()).padStart(2, '0');
+              var mm = String(date.getMonth() + 1).padStart(2, '0');    
+              var yyyy = date.getFullYear();
+              var hour = date.getHours();
+              var minutes = date.getMinutes();
+              var second = date.getSeconds();
+              date = mm + '/' + dd + '/' + yyyy + ' ' + hour + ':' + minutes + ":" + second;*/
+
+
+              document.getElementById("ayylmao").innerHTML+=" <li class='list-group-item'> Problem: "+ date+" <button type='button' class='btn btn-primary' href='/problem?id=" +id+"'>"+"Solve"+"</button></li>";
            }
          })
